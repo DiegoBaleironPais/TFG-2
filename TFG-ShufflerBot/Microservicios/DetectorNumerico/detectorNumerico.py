0c5f6da0b2aa553652482077d4d592f6806b3d0e
DiegoBaleironPais <121939378+DiegoBaleironPais@users.noreply.github.com>
2023-09-25 16:17:36 +0200

    Update del Microservicio
    Ahora funciona tambien con maquinas externas

--- TFG-ShufflerBot/Microservicios/DetectorNumerico/detectorNumerico.py
+++ TFG-ShufflerBot/Microservicios/DetectorNumerico/detectorNumerico.py
@@ -15,30 +15,36 @@ print("Modelo cargado con exito!!!!")
 def predict():
     # Checkeo de argumentos
     if 'file' not in request.files:
-        return jsonify({'error': 'No se ha enviado ninguna imagen'}), 400
+        # Si la request no contiene un parametro file, se vuelve un error
+        return jsonify({'Error': 'Este servicio necesita un parametro "file" en la request'}), 400
 
     file = request.files['file']
     if file.filename == '':
-        return jsonify({'error': 'No se ha seleccionado ninguna imagen'}), 400
+        # Si el parámetro file esta vacío se devuelve un error
+        return jsonify({'Error': 'El parametro file esta vacio'}), 400
 
     try:
-        # Lectura y preprocesado de la imagen
+        # Preprocesado de la imagen
         image = Image.open(file).resize((48, 53))
         image = np.array(image).reshape((-1, 48, 53, 1))
         image = image / 255.0
 
         # Prediccion
         pred = model.predict(image)
+        # Transformacion de la prediccion
         number = np.argmax(pred, axis=1)[0] + 1
         if number > 7:
             number += 2
-
+        
+        # Log + Respuesta al cliente
         print("Número leido con exito, ", number)
         return jsonify({'number': int(number)})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-#Inicio de la aplicacion
+# Inicio de la aplicacion
 if __name__ == '__main__':
-    app.run(debug=True)
+    #IMPORTANTE el parametro host que permite recibir peticiones de otras maquinas
+    app.run(host='0.0.0.0', debug=True)
+
