208206720e43f499a215b227d03a6217c2d5aab5
DiegoBaleironPais <121939378+DiegoBaleironPais@users.noreply.github.com>
2023-11-07 02:25:52 +0100

    Update pruebaDiego.py

--- TFG-ShufflerBot/Código ShufflerBot/shufflerbot/pruebaDiego.py
+++ TFG-ShufflerBot/Código ShufflerBot/shufflerbot/pruebaDiego.py
@@ -17,7 +17,7 @@ controller = telemetrix.Telemetrix()
 controller.set_pin_mode_digital_output(ENABLE_PIN)
 controller.digital_write(ENABLE_PIN, 0)
 
-
+"""
 try:
     # Referencia a la placa arduino del dispensador
     dispenser_serial = serial.Serial('/dev/ttyACM1', 9600)
@@ -25,7 +25,7 @@ try:
 except serial.SerialException as e:
     print(f"No se pudo abrir el puerto seriall: {e}")
     sys.exit(1)
-
+"""
 
 # Create the motors
 storage_motor = Motor(controller, STORAGE_PULSE_PIN, STORAGE_DIRECTION_PIN, STORAGE_NUM_STEPS, STORAGE_MAX_SPEED, STORAGE_ACCELERATION)
