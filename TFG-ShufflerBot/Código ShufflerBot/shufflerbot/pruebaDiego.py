cdadaa2b0fc40e4f8086981fe22b5ea45a5136d4
DiegoBaleironPais <121939378+DiegoBaleironPais@users.noreply.github.com>
2023-11-12 15:21:52 +0100

    Update pruebaDiego.py

--- TFG-ShufflerBot/Código ShufflerBot/shufflerbot/pruebaDiego.py
+++ TFG-ShufflerBot/Código ShufflerBot/shufflerbot/pruebaDiego.py
@@ -17,7 +17,7 @@ controller = telemetrix.Telemetrix(com_port='/dev/ttyACM0')  # Reemplaza con el 
 controller.set_pin_mode_digital_output(ENABLE_PIN)
 controller.digital_write(ENABLE_PIN, 0)
 
-"""
+
 try:
     # Referencia a la placa arduino del dispensador
     dispenser_serial = serial.Serial('/dev/ttyACM1', 9600)
@@ -25,7 +25,7 @@ try:
 except serial.SerialException as e:
     print(f"No se pudo abrir el puerto seriall: {e}")
     sys.exit(1)
-"""
+
 
 # Create the motors
 storage_motor = Motor(controller, STORAGE_PULSE_PIN, STORAGE_DIRECTION_PIN, STORAGE_NUM_STEPS, STORAGE_MAX_SPEED, STORAGE_ACCELERATION)
@@ -57,14 +57,13 @@ numCarta = 1
 try:
     for carta in cartas:
         input("Inserta la carta {numCarta}: ")
-        #dispenser_serial.write(b'p')
+        dispenser_serial.write(b'p')
         #resultadoInsercion = storage.insertion_wait()
         storage.insert_next_card(carta, numCarta, numCarta)
-        #dispenser_serial.reset_input_buffer()
+        dispenser_serial.reset_input_buffer()
         numCarta += 1
 finally:
-    #dispenser_serial.close()
-    print("fin")
+    dispenser_serial.close()
 
 
 
