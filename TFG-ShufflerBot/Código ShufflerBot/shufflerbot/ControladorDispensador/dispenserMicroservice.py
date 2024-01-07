5e8cfa05cdcd4f295fee1a3c913aaea40530a6ee
DiegoBaleironPais <121939378+DiegoBaleironPais@users.noreply.github.com>
2024-01-07 22:19:11 +0100

    d

--- TFG-ShufflerBot/Código ShufflerBot/shufflerbot/ControladorDispensador/dispenserMicroservice.py
+++ TFG-ShufflerBot/Código ShufflerBot/shufflerbot/ControladorDispensador/dispenserMicroservice.py
@@ -29,11 +29,36 @@ def estado():
 @app.route('/describe', methods=['GET'])
 def describe():
     descripcion = {
-        '/dispensar': 'Envía el comando para dispensar una carta',
-        '/encender': 'Envía el comando para encender el motor',
-        '/apagar': 'Envía el comando para apagar el motor',
-        '/estado': 'Devuelve el estado actual del motor (encendido o apagado)'
+        '/dispensar': {
+            'metodo': 'POST',
+            'descripcion': 'Envía el comando para dispensar una carta.',
+            'url': '/dispensar',
+            'parametros_entrada': None,
+            'parametros_salida': {'mensaje': 'Carta dispensada'}
+        },
+        '/encender': {
+            'metodo': 'POST',
+            'descripcion': 'Envía el comando para encender el motor del dispensador.',
+            'url': '/encender',
+            'parametros_entrada': None,
+            'parametros_salida': {'mensaje': 'Motor encendido'}
+        },
+        '/apagar': {
+            'metodo': 'POST',
+            'descripcion': 'Envía el comando para apagar el motor del dispensador.',
+            'url': '/apagar',
+            'parametros_entrada': None,
+            'parametros_salida': {'mensaje': 'Motor apagado'}
+        },
+        '/estado': {
+            'metodo': 'GET',
+            'descripcion': 'Consulta el estado actual del motor del dispensador (encendido o apagado).',
+            'url': '/estado',
+            'parametros_entrada': None,
+            'parametros_salida': {'encendido': 'Estado actual del motor'}
+        }
     }
+    
     return jsonify(descripcion)
 
 if __name__ == '__main__':
